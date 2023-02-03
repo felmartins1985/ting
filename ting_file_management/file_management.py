6f6d9ce6462ae165aeaaa5c53142466eb7ae27b8
hp <hp@pop-os.localdomain>
2023-02-03 15:30:42 -0300

    feat: requisito 2- funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return print(ValueError('Formato inválido'), file=sys.stderr)
+    try:
+        with open(path_file, mode='r') as file_text:
+            text_list = [line.replace('\n', '') for line in file_text]
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+    else:
+        return text_list
