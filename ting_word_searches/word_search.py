5ae62f60a2879033d47307bf83fd418cb83ca539
hp <hp@pop-os.localdomain>
2023-02-06 11:44:08 -0300

    feat: requisito 8- search_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    show_return_exists = []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        show_lines_word = []
+        for index, line in enumerate(data["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                show_lines_word.append(
+                    {
+                        "linha": index + 1,
+                        "conteudo": line,
+                    }
+                )
+        if len(show_lines_word) > 0:
+            show_return_exists.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": show_lines_word,
+                }
+            )
+    return show_return_exists
