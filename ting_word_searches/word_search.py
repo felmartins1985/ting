394a818bc11e31d70313de6211cb73e9bfd4b447
hp <hp@pop-os.localdomain>
2023-02-06 15:21:49 -0300

    refactor: criando duas funcoes a parte para facilitar o clean code dos requisitos 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,23 +1,33 @@
+from ting_file_management.queue import Queue
+from ting_file_management.file_process import process
+
+
+def generate_word_report(word, file, occurrences):
+    return {"palavra": word, "arquivo": file, "ocorrencias": occurrences}
+
+
+def create_word_report(word, file, text=False):
+    lines_search = []
+    for index, line in enumerate(file["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            lines_text = {
+                "linha": index + 1,
+            }
+            if text:
+                lines_text["conteudo"] = line
+            lines_search.append(lines_text)
+    return lines_search
+
+
 def exists_word(word, instance):
     show_return_exists = []
     for index in range(len(instance)):
         data = instance.search(index)
-        show_lines_word = []
-        for index, line in enumerate(data["linhas_do_arquivo"]):
-            if word.lower() in line.lower():
-                show_lines_word.append(
-                    {
-                        "linha": index + 1,
-                    }
-                )
-        if len(show_lines_word) > 0:
-            show_return_exists.append(
-                {
-                    "palavra": word,
-                    "arquivo": data["nome_do_arquivo"],
-                    "ocorrencias": show_lines_word,
-                }
-            )
+        lines_search = create_word_report(word, data)
+        if len(lines_search) > 0:
+            file_path = data["nome_do_arquivo"]
+            result = generate_word_report(word, file_path, lines_search)
+            show_return_exists.append(result)
     return show_return_exists
 
 
@@ -25,21 +35,10 @@ def search_by_word(word, instance):
     show_return_exists = []
     for index in range(len(instance)):
         data = instance.search(index)
-        show_lines_word = []
-        for index, line in enumerate(data["linhas_do_arquivo"]):
-            if word.lower() in line.lower():
-                show_lines_word.append(
-                    {
-                        "linha": index + 1,
-                        "conteudo": line,
-                    }
-                )
-        if len(show_lines_word) > 0:
-            show_return_exists.append(
-                {
-                    "palavra": word,
-                    "arquivo": data["nome_do_arquivo"],
-                    "ocorrencias": show_lines_word,
-                }
-            )
+        lines_search = []
+        lines_search = create_word_report(word, data, True)
+        if len(lines_search) > 0:
+            file_path = data["nome_do_arquivo"]
+            result = generate_word_report(word, file_path, lines_search)
+            show_return_exists.append(result)
     return show_return_exists
